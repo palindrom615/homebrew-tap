68f5dac53f334f98731c7e231a623d6aa3afcdc8
goreleaserbot <goreleaser@carlosbecker.com>
2021-01-26 08:18:15 +0900

    Brew formula update for sdkman version v0.4.13

--- Formula/sdkman.rb
+++ Formula/sdkman.rb
@@ -5,16 +5,16 @@
 class Sdkman < Formula
   desc "A tool for managing parallel versions of multiple Software Development Kits"
   homepage "https://github.com/palindrom615/sdkman"
-  version "0.4.12"
+  version "0.4.13"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/palindrom615/sdkman/releases/download/v0.4.12/sdkman_0.4.12_Darwin_x86_64.tar.gz"
-    sha256 "4506230a1fe4c445eec08129007e7adb637e02d274450f2c30e681ec985d42bf"
+    url "https://github.com/palindrom615/sdkman/releases/download/v0.4.13/sdkman_0.4.13_Darwin_x86_64.tar.gz"
+    sha256 "3efbe28c03b40a8a91dac461cd9cdf2b6a8b36f4f40793ac50bc7e225898ee22"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/palindrom615/sdkman/releases/download/v0.4.12/sdkman_0.4.12_Linux_x86_64.tar.gz"
-    sha256 "d8abf68ed9738832e2e8ca505cf2716ba1334513a75c627d1db9891c6dd16132"
+    url "https://github.com/palindrom615/sdkman/releases/download/v0.4.13/sdkman_0.4.13_Linux_x86_64.tar.gz"
+    sha256 "b09a40700414619fc874bc2b8925c82bf66385bb246df8c0a31e88fc14604c28"
   end
 
   def install
